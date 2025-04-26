48de03e9cb41192d2b3b92332cc0d195ce03abb4
cgcerro <cgcerro@gmail.com>
2025-04-26 13:18:41 +0200

    arrow animation

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 window.addEventListener('load', function() {
+    videoPlay();
+    arrowMobile();    
+});
+  
+
+videoPlay = () => {
     var video = document.getElementById('logo-video');
     video.play();
+}
+
+arrowMobile = () => {
+  const arrowMobile = document.getElementById('arrow-mobile');
+  const initialViewMobile = document.getElementById('about-mobile-primary');
+  const secondaryViewMobile = document.getElementById('about-mobile-secondary');
+
+  arrowMobile.addEventListener('click', (event) => {
+    event.preventDefault();
+    initialViewMobile.style.display = 'none'; // Oculta título y flecha
+    secondaryViewMobile.classList.add('active'); // Muestra el contenido desplazándose
   });
-  
+}  
